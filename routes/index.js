72b8eb810c89047fe4978bb58f85882d03190b81
Christopher Violante <christopherviolante@Christophers-MacBook-Pro.local>
2020-05-29 15:52:32 -0600

    oAuth enabled and running.  User model in place

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// will need to add isLoggedIn inbetween route and controller in routes
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) return next();
+  res.redirect('/auth/google');
+};
 
 module.exports = router;
